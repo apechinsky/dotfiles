58ee7e17976e405f621b20f91961f551a1ca7bf2
Anton Pechinsky <anton@pechinsky.com>
2014-01-02 17:14:24 +0300

    Merge home and work configs

--- install.sh
+++ install.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
-#FILES=~/dotfiles/*
+# This dir
+SCRIPT_DIR=~/dotfiles
+
+# Target dir (where to create symbolic links)
+TARGET_DIR=~
+
+# Create dir to store old configuration files
+[ ! -d '$SCRIPT_DIR/old' ] && mkdir $SCRIPT_DIR/old
+
+# Managed configuration files
 FILES="
 .vimrc
 .zshrc
 .conkyrc
+vrc
 .i3"
-
-TARGET=~/temp/dotfiles
 
 for f in $FILES
 do
-   ln -s ~/dotfiles/$f $TARGET/$f
+   # Create symlink for each managed file
+   if [ -f "$f" ]; then
+       echo "Save file '$TARGET_DIR/$f' to '$SCRIPT_DIR/old/$f'"
+       mv $TARGET_DIR/$f $SCRIPT_DIR/old/$f 
+
+       echo "Creating symlink '$TARGET_DIR/$f' -> '$SCRIPT_DIR/$f'"
+       ln -s $SCRIPT_DIR/$f $TARGET_DIR/$f
+   fi
+
+   # do not touch symlinks
+   [ -L "$f" ] && echo "File '$TARGET_DIR/$f' is symlink. Skipping..."
 done
 
 
