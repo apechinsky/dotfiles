ab8622ca06916d370caff88f03cbf3bcd714bc2c
apechinsky <apechinsky@qulix.com>
2023-01-16 09:31:29 +0300

    add missing colors.lua file

--- .config/nvim/lua/anton/colors.lua
+++ .config/nvim/lua/anton/colors.lua
@@ -1,1 +1,13 @@
+function FixHabamaxColors()
+    vim.cmd([[highlight SignifySignAdd ctermfg=green  guifg=#008800 cterm=NONE gui=NONE]])
+    vim.cmd([[highlight MatchParen ctermfg=green  guifg=#009900 cterm=NONE gui=NONE]])
+end
+
+vim.cmd([[
+    augroup FixHabamaxColors
+        autocmd!
+        autocmd ColorScheme habamax call v:lua.FixHabamaxColors()
+    augroup end
+]])
+
 vim.cmd([[colorscheme habamax]])
