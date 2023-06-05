6641390fbb20903b8a96807561e0fd24b9cb2481
Anton Pechinsky <anton@pechinsky.com>
2023-06-05 07:16:25 +0300

    Extend config.lua with new methods

--- .config/nvim/lua/anton/config.lua
+++ .config/nvim/lua/anton/config.lua
@@ -1,19 +1,43 @@
 local config = {
 
-    path = vim.fn.stdpath('config')
+    config = vim.fn.stdpath('config'),
+
+    data = vim.fn.stdpath('data'),
+
+    cache = vim.fn.stdpath('cache'),
+
+    configPath = function(path)
+        return subpath(config.config, path)
+    end
 
 }
 
-local function subpath(path, sub)
-    local separator = string.sub(path, #path, #path) == '/'
-        and ''
-        or '/'
+local function removeTrailing(path, char)
+    return string.sub(path, #path, #path) == char
+        and string.sub(path, 1, #path - 1)
+        or path
+end
 
-    return path .. separator .. sub
+local function removeLeading(path, char)
+    return string.sub(path, 1, 1) == char
+        and string.sub(path, 2, #path)
+        or path
+end
+
+local function removeTrailingSlash(path)
+    return removeTrailing(path, '/')
+end
+
+local function removeLeadingSlash(path)
+    return removeLeading(path, '/')
+end
+
+local function subpath(path, sub)
+    return removeTrailingSlash(path) .. "/" .. removeLeadingSlash(sub)
 end
 
 function config.subpath(sub)
-    return subpath(config.path, sub)
+    return subpath(config.config, sub)
 end
 
 function config.tools(sub)
