2f5bbe3d2c03e6ce40b62ed3819b3add8fb96585
apechinsky <apechinsky@qulix.com>
2023-09-17 09:02:34 +0300

    Refactor jdtls configuration

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -58,165 +58,170 @@ local workspace_dir = getWorkspaceDir(project:get_name())
 -- vim.diagnostic.enable()
 
 -- is this needed???
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 --
-local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-    --
-    cmd = {
-        utils.child(HOME, 'opt/jdk-17/bin/java'),
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-javaagent:' .. vim.fn.glob(xdg.config('java/lombok-*.jar')),
-        -- '-javaagent:' .. vim.fn.glob(xdg.config('java/springloaded-*.jar')),
-        '-Xms2g',
-        -- '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar',  vim.fn.glob(utils.child(jdtls_home, 'plugins/org.eclipse.equinox.launcher_*.jar')),
-        '-configuration', utils.child(jdtls_home, "config_linux"),
-        '-data', workspace_dir,
-    },
+local extendedClientCapabilities = jdtls.extendedClientCapabilities;
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true;
 
-    -- This is the default if not provided, you can remove it. Or adjust as needed.
+local config = {
     -- One dedicated LSP server & client will be started per unique root_dir
     root_dir = project:get_root_dir(),
 
     -- Here you can configure eclipse.jdt.ls specific settings
     -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    settings = {
-
-        -- see JavaConfiguration class
-        java = {
-            home = HOME .. '/opt/jdk-17',
-            eclipse = {
-                downloadSources = true,
-            },
-            configuration = {
-                updateBuildConfiguration = "interactive",
-                runtimes = {
-                    {
-                        name = "JavaSE-11",
-                        path = utils.child(HOME, "/opt/jdk-11"),
-                    },
-                    {
-                        name = "JavaSE-19",
-                        path = utils.child(HOME, "/opt/jdk-19"),
-                    },
-                    {
-                        name = "JavaSE-18",
-                        path = utils.child(HOME, "/opt/jdk-18"),
-                    },
-                    {
-                        name = "JavaSE-17",
-                        path = utils.child(HOME, "/opt/jdk-17"),
-                    },
-                }
-            },
-            jdt = {
-                ls = {
-                    lombokSupport = true,
-                },
-            },
-            maven = {
-                downloadSources = true,
-            },
-            implementationsCodeLens = {
-                enabled = true,
-            },
-            referencesCodeLens = {
-                enabled = true,
-            },
-            references = {
-                includeDecompiledSources = true,
-            },
-            format = {
-                enabled = true,
-                settings = {
-                    -- url = vim.fn.stdpath("config") .. "/tools/intellij-java-google-style.xml",
-                    url = xdg.config('/tools/eclipse-my.xml'),
-                    profile = "eclipse-my",
-                },
-            },
-            trace = {
-                server = 'verbose',
-            },
-            import = {
-                gradle = {
-                    enabled = true,
-                },
-                maven = {
-                    enabled = true,
-                },
-                exclusions = {
-                    "**/node_modules/**",
-                    "**/.metadata/**",
-                    "**/archetype-resources/**",
-                    "**/META-INF/maven/**",
-                    "/**/test/**",
-                 },
-            },
-            signatureHelp = { enabled = true },
-            completion = {
-                favoriteStaticMembers = {
-                    "org.hamcrest.MatcherAssert.assertThat",
-                    "org.hamcrest.Matchers.*",
-                    "org.hamcrest.CoreMatchers.*",
-                    "org.junit.jupiter.api.Assertions.*",
-                    "java.util.Objects.requireNonNull",
-                    "java.util.Objects.requireNonNullElse",
-                    "org.mockito.Mockito.*",
-                    "org.slf4j.Logger",
-                    "org.slf4j.LoggerFactory",
-                    "org.srplib.contract.Argument",
-                    "org.srplib.contract.Assert"
-                },
-                importOrder = {
-                    "java", "#java",
-                    "javax", "#javax",
-                    "org", "#org",
-                    "net", "#net",
-                    "com", "#com",
-                    "by", "#by",
-                    "ru", "#ru",
-                    "", "#",
-                },
-            },
-            sources = {
-                organizeImports = {
-                    starThreshold = 9999,
-                    staticStarThreshold = 9999,
-                },
-            },
-            codeGeneration = {
-                toString = {
-                    template = "${object.className} [${member.name()}: ${member.value}, ${otherMembers}]",
-                },
-                useBlocks = true,
-            },
-        },
-        -- not sure this is needed
-        -- capabilities = capabilities,
-        -- not sure this is needed
-        -- extendedClientCapabilities = extendedClientCapabilities,
-    },
 
     flags = {
         allow_incremental_sync = true,
     },
+
     init_options = {
+        extendedClientCapabilities = extendedClientCapabilities,
         bundles = {
             vim.fn.glob(utils.child(HOME, 'java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar'))
+        },
+    },
+
+    capabilities = capabilities,
+}
+
+-- The command that starts the language server
+-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+config.cmd = {
+    utils.child(HOME, 'opt/jdk-17/bin/java'),
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    -- '-javaagent:' .. vim.fn.glob(xdg.config('java/lombok-*.jar')),
+    -- '-javaagent:' .. vim.fn.glob(xdg.config('java/springloaded-*.jar')),
+    '-Xms4g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '-jar',  vim.fn.glob(utils.child(jdtls_home, 'plugins/org.eclipse.equinox.launcher_*.jar')),
+    '-configuration', utils.child(jdtls_home, "config_linux"),
+    '-data', workspace_dir,
+}
+
+config.settings = {
+
+    -- see JavaConfiguration class
+    java = {
+        home = HOME .. '/opt/jdk-17',
+        eclipse = {
+            downloadSources = true,
+        },
+        configuration = {
+            updateBuildConfiguration = "interactive",
+            runtimes = {
+                {
+                    name = "JavaSE-11",
+                    path = utils.child(HOME, "/opt/jdk-11"),
+                },
+                {
+                    name = "JavaSE-19",
+                    path = utils.child(HOME, "/opt/jdk-19"),
+                },
+                {
+                    name = "JavaSE-18",
+                    path = utils.child(HOME, "/opt/jdk-18"),
+                },
+                {
+                    name = "JavaSE-17",
+                    path = utils.child(HOME, "/opt/jdk-17"),
+                },
+            }
+        },
+        jdt = {
+            ls = {
+                lombokSupport = true,
+            },
+        },
+        maven = {
+            downloadSources = true,
+        },
+        implementationsCodeLens = {
+            enabled = true,
+        },
+        referencesCodeLens = {
+            enabled = true,
+        },
+        references = {
+            includeDecompiledSources = true,
+        },
+        format = {
+            enabled = true,
+            settings = {
+                -- url = vim.fn.stdpath("config") .. "/tools/intellij-java-google-style.xml",
+                url = xdg.config('/tools/eclipse-my.xml'),
+                profile = "eclipse-my",
+            },
+        },
+        trace = {
+            server = 'verbose',
+        },
+        import = {
+            gradle = {
+                enabled = true,
+            },
+            maven = {
+                enabled = true,
+            },
+            exclusions = {
+                "**/node_modules/**",
+                "**/.metadata/**",
+                "**/archetype-resources/**",
+                "**/META-INF/maven/**",
+                "/**/test/**",
+             },
+        },
+        signatureHelp = { enabled = true },
+        completion = {
+            favoriteStaticMembers = {
+                "org.hamcrest.MatcherAssert.assertThat",
+                "org.hamcrest.Matchers.*",
+                "org.hamcrest.CoreMatchers.*",
+                "org.junit.jupiter.api.Assertions.*",
+                "java.util.Objects.requireNonNull",
+                "java.util.Objects.requireNonNullElse",
+                "org.mockito.Mockito.*",
+                "org.slf4j.Logger",
+                "org.slf4j.LoggerFactory",
+                "org.srplib.contract.Argument",
+                "org.srplib.contract.Assert"
+            },
+            importOrder = {
+                "java", "#java",
+                "javax", "#javax",
+                "org", "#org",
+                "net", "#net",
+                "com", "#com",
+                "by", "#by",
+                "ru", "#ru",
+                "", "#",
+            },
+        },
+        sources = {
+            organizeImports = {
+                starThreshold = 9999,
+                staticStarThreshold = 9999,
+            },
+        },
+        codeGeneration = {
+            toString = {
+                template = "${object.className} [${member.name()}: ${member.value}, ${otherMembers}]",
+            },
+            useBlocks = true,
         },
     },
 }
 
 config.on_attach = function(client, bufnr)
+
+    -- jdtls should apply this command automatically but no idea why it don't
+    jdtls.setup.add_commands()
 
     jdtls.setup_dap({ hotcodereplace = 'auto' })
 
