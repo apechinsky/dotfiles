af1c385dbb2c8f6a7f9390c3102d57f49f8a9e92
apechinsky <apechinsky@qulix.com>
2024-08-28 22:25:33 +0300

    vim. tweak nvim-tree

--- .config/nvim/lua/anton/plugins/nvim-tree.lua
+++ .config/nvim/lua/anton/plugins/nvim-tree.lua
@@ -4,12 +4,19 @@ return {
     opts = {
         sync_root_with_cwd = false,
 
+        auto_reload_on_write = true,
+
         view = {
             width = 40,
         },
 
         filters = {
-          dotfiles = true,
+            dotfiles = true,
+        },
+
+        notify = {
+            threshold = vim.log.levels.WARNING,
+            absolute_path = true,
         },
     }
 }
