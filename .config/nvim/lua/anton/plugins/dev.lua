267f2df4f562fc4bfc204b795a9e817175454ec7
apechinsky <apechinsky@qulix.com>
2025-03-02 11:53:57 +0300

    Move plugin apechinsky/slides.nvim to slides.lua. Add comments to dev.lua

---
+++ .config/nvim/lua/anton/plugins/dev.lua
@@ -0,0 +1,11 @@
+-- Demonstrates how to develop a plugin locally
+return {
+    -- Development: Specify plugin location dir
+    -- dir = '~/neovim-plugins/myplugin.nvim',
+    -- Production: Github plugin location
+    -- 'myaccount/slides.nvim',
+    --
+    -- config = function()
+    --     local slides = require('myaccount/myplugin')
+    -- end,
+}
---
+++ .config/nvim/lua/anton/plugins/slides.lua
@@ -0,0 +1,9 @@
+return {
+    'apechinsky/slides.nvim',
+    -- dir = '~/work/my/github/neovim-plugins/slides.nvim',
+    --
+    -- config = function()
+    --     local slides = require('apechinsky/slides')
+    --     slides.start()
+    -- end,
+}
